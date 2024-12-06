1d6e5f32e70afa21e4c96340b81050bbc16a9e8a
Gabriel-Assis03 <gabrieleboltffggff@gmail.com>
2024-12-05 22:13:17 -0300

    base da home page e botao de mudar de pag

--- price/views.py
+++ price/views.py
@@ -4,6 +4,7 @@ from .controller import coins_options, caculate_value
 
 def exchange_page(request):
     data = coins_options()
+    finalValue = ''
     if request.method == "POST":
         coinNow = request.POST['coin-now']
         coinNew = request.POST['coin-new']
@@ -23,5 +24,9 @@ def exchange_page(request):
     return render(request, 'exchange_page.html', context)
 
 def home_page(request):
-    return render(request, 'home_page.html')
-
+    context={
+        'dolar': caculate_value(1,'USD','BRL'),
+        'euro': caculate_value(1,'EUR','BRL'),
+        'bitcoin': caculate_value(1,'BTC','BRL')
+    }
+    return render(request, 'home_page.html', context)
